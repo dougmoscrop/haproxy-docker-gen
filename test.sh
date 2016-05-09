a579a32e38d7ad5c8980c348d34a89b72ff7e498
Doug Moscrop <Doug.Moscrop@d2l.com>
2016-05-08 21:34:34 -0400

    Slight changes to trigger docker hub build

--- test.sh
+++ test.sh
@@ -2,7 +2,9 @@
 set -e
 
 if curl -s http://hello.127.0.0.1.xip.io | grep -q "<h1>Hello world!</h1>"; then
+  echo "All good!"
   exit 0;
 else
+  echo "Did not receive expected response!"
   exit 1;
 fi
