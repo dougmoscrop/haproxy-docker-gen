587618d22c4bafe4911a522f97b31a4e999b046e
Doug Moscrop <Doug.Moscrop@d2l.com>
2016-05-08 21:18:45 -0400

    Fix CI

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if curl -s http://hello.169.0.13.37.xip.io | grep -q "<h1>Hello world!</h1>"; then
+if curl -s http://hello.127.0.0.1.xip.io | grep -q "<h1>Hello world!</h1>"; then
   exit 0;
 else
   exit 1;
